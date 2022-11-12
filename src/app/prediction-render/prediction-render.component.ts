da7e38a0d45345d7d8e6e7cc2ef5d51d3f9fd911
Christian Scillitoe <christian.scillitoe@gmail.com>
2022-11-12 14:13:35 -0800

    Convert render to variables for brad

--- src/app/prediction-render/prediction-render.component.ts
+++ src/app/prediction-render/prediction-render.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prediction-render.component.scss']
 })
 export class PredictionRenderComponent implements OnInit {
+  predictionTitle: string = "Will the banana win this game?";
+
+  believeChoiceText: string = "Yes";
+  believePayoutMultiplier: string = "1.29";
+  believeTotalPoints: string = "2500";
+
+  doubtChoiceText: string = "No";
+  doubtPayoutMultiplier: string = "2.4";
+  doubtTotalPoints: string = "500";
+
+  believePercentage: number = 60;
+  doubtPercentage: number = 40;
+
+  predictionTimer: string = "00:00";
 
   constructor() { }
 
