8a9bc3b93f57109292c1d049749d4a4b1aca1f8c
BrendanFromBC <132005870+BrendanFromBC@users.noreply.github.com>
2023-05-04 20:15:21 -0700

    added overlay-box and overlay-title component

---
+++ src/app/overlay-box/overlay-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-overlay-box',
+  templateUrl: './overlay-box.component.html',
+  styleUrls: ['./overlay-box.component.scss']
+})
+export class OverlayBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
