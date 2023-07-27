167955b8f6c7ddd5372576515db14ab8bab440f7
crscillitoe <you@example.com>
2023-07-27 21:19:17 +0000

    New branch for rank icon

---
+++ src/app/stream-layout/stream-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stream-layout',
+  templateUrl: './stream-layout.component.html',
+  styleUrls: ['./stream-layout.component.scss']
+})
+export class StreamLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
