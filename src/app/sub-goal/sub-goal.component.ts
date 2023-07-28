cd38af8881af2a26a4f6df831ef16d14389e14b9
crscillitoe <you@example.com>
2023-07-28 00:13:03 +0000

    new sub notifs

--- src/app/sub-goal/sub-goal.component.ts
+++ src/app/sub-goal/sub-goal.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { getBaseStreamURL } from '../utility';
+import { Howl } from 'howler';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-sub-goal',
   templateUrl: './sub-goal.component.html',
-  styleUrls: ['./sub-goal.component.scss']
+  styleUrls: ['./sub-goal.component.scss'],
+  animations: [
+    trigger('slideUp', [
+      transition(':enter', [
+          style({'padding-top': '154px'}),
+          animate('1s ease-out', style({'padding-top': '0px'}))
+      ]),
+
+      transition(':leave',
+        animate('1s ease-out', style({'padding-top': '154px'}))
+      )
+    ])
+  ]
 })
 export class SubGoalComponent implements OnInit {
   tier1: number = 0;
@@ -12,10 +26,20 @@ export class SubGoalComponent implements OnInit {
   tier3: number = 0;
 
   total: number = 1000;
-
   goal: number = 1500;
 
-  constructor() { }
+  displaySub: boolean = false;
+  subMessage: string = "THANK YOU WOOHOOJIN FOR THE T3 I REALLY APPRECIATE IT DUDE GOOD LOOKS NICE JOB YOU'RE A KING";
+  subHowl: Howl;
+  lastTimeout: NodeJS.Timeout | undefined;
+
+  constructor() {
+    this.subHowl = new Howl({
+      src: ["assets/ChossBoss.wav"],
+      autoplay: false,
+      loop: false
+    });
+  }
 
   getPercentage() {
     return (this.total / this.goal) * 100;
@@ -23,7 +47,10 @@ export class SubGoalComponent implements OnInit {
 
   ngOnInit(): void {
     const streamURL = getBaseStreamURL() + "?channel=subs-count"
+    const subNotifStreamURL = getBaseStreamURL() + "?channel=subs"
     var source = new EventSource(streamURL);
+    var subSource = new EventSource(subNotifStreamURL);
+
     source.addEventListener('open', (e) => {
       console.log("The connection has been established.");
     });
@@ -38,6 +65,41 @@ export class SubGoalComponent implements OnInit {
       this.total = this.tier1 + this.tier2 + this.tier3;
     }, false);
     source.addEventListener('error', function (event) {
+      console.log(event)
+    }, false);
+
+
+    subSource.addEventListener('open', (e) => {
+      console.log("The connection has been established.");
+
+      if (this.lastTimeout) {
+        clearTimeout(this.lastTimeout);
+      }
+    });
+
+    subSource.addEventListener('publish', (event) => {
+      if (this.lastTimeout) {
+        clearTimeout(this.lastTimeout);
+      }
+
+      var data = JSON.parse(event.data);
+      this.subMessage = data.message;
+      let timeout = 5000;
+
+      this.total++;
+
+      this.displaySub = true;
+      this.subHowl.stop();
+      this.subHowl.play();
+      this.subHowl.fade(0.6, 0, timeout)
+
+
+      this.lastTimeout = setTimeout(() => {
+        this.displaySub = false;
+      }, timeout);
+    }, false);
+
+    subSource.addEventListener('error', function (event) {
       console.log(event)
     }, false);
   }
