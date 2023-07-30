ea4d16e2aef8bea4347770828a0b93c56079cf49
bwbonanno <bradfordbonanno@gmail.com>
2023-07-30 01:26:00 +0000

    CUSTOM EMOJI RENDERING

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,6 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { BotConnectorService } from '../services/bot-connector.service';
 
+enum ChatChunkType {
+  TEXT = 0,
+  IMG = 1
+}
+
+interface ChatChunk {
+  type: ChatChunkType;
+  content: string;
+}
+
+interface ChatMessage {
+  chunks: ChatChunk[];
+  textChunkCount: number;
+  imgChunkCount: number;
+}
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -50,13 +65,78 @@ export class ChatComponent implements OnInit {
         }
       }
 
-      console.log(data);
+      const chatMessage = this.processEmoijs(data.content, data.emojis)
+
+      data.chatMessage = chatMessage;
+
       this.messages.push(data);
       if (this.messages.length > this.QUEUE_LENGTH) {
         this.messages.shift();
       }
     });
+
   }
 
+  processEmoijs(messageContent: string, emojiContent: { [key: string]: string }[]): ChatMessage {
+    const chatChunks: ChatChunk[] = [];
+    const emojiMap = new Map<string, string>();
+
+    let updatedMessageContent = messageContent;
+
+    emojiContent.forEach(emoji => {
+      const emojiText = emoji["emoji_text"]
+      emojiMap.set(emojiText, emoji["emoji_url"])
+      // Handle issues of no space before the emoji name
+      updatedMessageContent = updatedMessageContent.replaceAll(emojiText, ` ${emojiText} `)
+    });
+
+    const splitMessage = updatedMessageContent.split(" ");
+    let currentTextChunk = "";
+    let imgChunkCount = 0;
+    let textChunkCount = 0;
+    splitMessage.forEach(wordChunk => {
+      if (emojiMap.has(wordChunk)) {
+        if (currentTextChunk.trim() !== "") {
+          chatChunks.push(
+            {
+              "type": ChatChunkType.TEXT,
+              "content": currentTextChunk.trim()
+            }
+          );
+          currentTextChunk = "";
+          textChunkCount++;
+        }
+        const url = emojiMap.get(wordChunk)!;
+        chatChunks.push(
+          {
+            "type": ChatChunkType.IMG,
+            "content": url
+          }
+        )
+        imgChunkCount++;
+      } else {
+        currentTextChunk += wordChunk + " ";
+      }
+    })
+
+    if (currentTextChunk.trim() !== "") {
+      chatChunks.push(
+        {
+          "type": ChatChunkType.TEXT,
+          "content": currentTextChunk.trim()
+        }
+      )
+      textChunkCount++;
+    }
+    return {
+      chunks: chatChunks,
+      imgChunkCount,
+      textChunkCount
+    }
+  }
+
+  public get ChatChunkType() {
+    return ChatChunkType;
+  }
 
 }
