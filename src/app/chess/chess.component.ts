3f89746f0c2d289afa0f8fc01a4b20f72e26f574
crscillitoe <you@example.com>
2023-07-31 20:45:11 +0000

    Update to angular 15

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Chessground } from 'chessground';
 
 @Component({
   selector: 'app-chess',
@@ -11,13 +10,6 @@ export class ChessComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    const config = {
-      fen: '2r3k1/pp2Qpbp/4b1p1/3p4/3n1PP1/2N4P/Pq6/R2K1B1R w -',
-    };
-
-    const board = document.getElementById('board')!;
-    const ground = Chessground(board, config as any);
-    console.log(ground.getFen());
   }
 
 }
