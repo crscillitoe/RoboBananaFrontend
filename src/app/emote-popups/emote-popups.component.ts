ec70b40d43005bbe324be672f9b18390937bd9cc
crscillitoe <you@example.com>
2024-03-10 17:13:11 +0000

    Add emoji bouncing feature

--- src/app/emote-popups/emote-popups.component.ts
+++ src/app/emote-popups/emote-popups.component.ts
@@ -12,39 +12,44 @@ import { ChatChunk, ChatChunkType, ChatProcessorService } from "../services/chat
 })
 export class EmotePopupsComponent implements OnInit {
 
-  //isTesting is being set from the testing.component when that is being initialized
   public static isTesting: boolean = false;
+
   static emojiRegex = new RegExp(/\p{Emoji_Presentation}/gu);
-
-
   constructor(private botService: BotConnectorService, private chatProcessorService: ChatProcessorService) { }
 
   @ViewChild(EmotePopupsDirective, { static: true }) emotePopupSpace!: EmotePopupsDirective;
-  @Input() name?: string; //for future shenanigans with multiple emote zones
-  @Input() direction: string = "up"; //the direction in which the emotes should jump // up || down || left || right
 
-  protected enabled: boolean = false; //whether chat messages should currently trigger emote jumps
-  protected duration: number = 1500; //jump duration in ms
-  protected size: number = 40; //the size of an emote blip in px (square) //a size of 40 or smaller is recommended
+  // This component is rendered 4 times, once for each direction
+  @Input() name?: string;
 
+  @Input() direction: "up" | "down" | "left" | "right" = "up";
+
+  // Whether chat messages should currently trigger emote jumps
+  protected enabled: boolean = false;
+
+  // Jump duration in ms
+  protected duration: number = 1500;
+
+  // the size of an emote blip in px (square) //a size of 40 or smaller is recommended
+  protected size: number = 40;
   ngOnInit(): void {
 
     this.botService.getStream("streamdeck").subscribe(data => {
 
-      if (data.type === "happy-emotes" && (data.name === this.name || data.name === "all")) {
-        
-        if (data.value != null) {
-          this.enabled = data.value;
-        }
+      if (data.type === "happy-emotes" && (data.location === this.name || data.location === "all")) {
+
         if (data.enabled != null) {
           this.enabled = data.enabled;
         }
+
         if (data.duration != null) {
           this.duration = data.duration;
         }
-        if (data.size != null) {
-          this.size = data.size;
+
+        if (data.emoteSize != null) {
+          this.size = data.emoteSize;
         }
+
         if (data.amount != null) {
           for (let index = 0; index < data.amount; index++) {
             this.createPopupEmote({imageUrl: "assets/cool.webp"});
@@ -54,25 +59,24 @@ export class EmotePopupsComponent implements OnInit {
     });
 
     this.botService.getStream("chat-message").subscribe(data => {
-
       if (!this.enabled)  return;
       if (data.content.length > 200) return;
-      
-      //creates a default emote from each message if we are in test mode
-      //done so fake messages trigger emotes to display
+
+      // creates a default emote from each message if we are in test mode
+      // done so fake messages trigger emotes to display
       if (EmotePopupsComponent.isTesting) this.createPopupEmote({imageUrl: "assets/cool.webp"});
-      
+
       const message = this.chatProcessorService.processChat(data, undefined, 0, 0);
       const emotes = message.chatMessage.chunks.filter((e: ChatChunk) => {return e.type === ChatChunkType.IMG});
-      
-      //block for identifying unicode emojis and rendering up to 4
+
+      // block for identifying unicode emojis and rendering up to 4
       let unicodeEmotes = [...message.content.matchAll(EmotePopupsComponent.emojiRegex)];
       unicodeEmotes = unicodeEmotes.slice(0, 4);
       unicodeEmotes.forEach(e => {
         this.createPopupEmote({textContent: e});
       });
 
-      //block for rendering discord emotes
+      // block for rendering discord emotes
       if (emotes.length > 0) {
         emotes.forEach((emote: ChatChunk) => {
           this.createPopupEmote({imageUrl: emote.content});
@@ -83,12 +87,10 @@ export class EmotePopupsComponent implements OnInit {
       if (message.stickerURL !== "") {
         this.createPopupEmote({imageUrl: message.stickerURL});
       }
-
     });
   }
 
   createPopupEmote(assets: {imageUrl?: string, textContent?: string}): void {
-
     const viewContainerRef = this.emotePopupSpace.viewContainerRef;
     const componentRef = viewContainerRef.createComponent<EmotePopupIconComponent>(EmotePopupIconComponent);
 
@@ -104,5 +106,4 @@ export class EmotePopupsComponent implements OnInit {
       componentRef.destroy();
     });
   }
-
 }
