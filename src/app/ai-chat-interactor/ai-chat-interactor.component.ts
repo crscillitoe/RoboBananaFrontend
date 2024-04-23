502673aa3868ae0635b272f06b0ca62ade337683
crscillitoe <you@example.com>
2024-04-23 16:32:22 +0000

    Adjust tts voice to turbo

--- src/app/ai-chat-interactor/ai-chat-interactor.component.ts
+++ src/app/ai-chat-interactor/ai-chat-interactor.component.ts
@@ -204,7 +204,7 @@ export class AiChatInteractorComponent implements OnInit {
         'xi-api-key': this.ELEVENLABS_KEY,
       };
 
-      const model = "eleven_multilingual_v2";
+      const model = "eleven_turbo_v2";
 
       const request = {
         "text": tts,
