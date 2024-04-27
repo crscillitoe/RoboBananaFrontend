d2f089740b52aa625552567cccaba4d6470e616e
ValpsZ <limelgren@gmail.com>
2024-04-27 21:12:31 +0200

    Add support for moving multiple spaces in one command

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -9,12 +9,17 @@ export class PokemonService {
   constructor(private http: HttpClient) { }
 
   playMove(move: string, amount: number) {
-    let moves: string[] = []
-    for (let i = 0; i < amount; i++) {
-      moves.push(move)
+    let frames: number = 1
+    switch (move) {
+      case "Up":
+      case "Down":
+      case "Left":
+      case "Right":
+        frames = 18 * amount; // 18 is the amount of frames it takes to move one space in Emerald
+        break
     }
 
-    this.http.post(`http://localhost:5000/mgba-http/button/tapmany?key=${moves.join(",")}`, {})
+    this.http.post(`http://localhost:5000/mgba-http/button/hold?key=${move}&duration=${frames}`)
       .subscribe(data => {})
   }
 }
