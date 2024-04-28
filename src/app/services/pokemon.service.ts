699b6db40bad75d09e6ca74fe3bb12d16e93b73a
ValpsZ <limelgren@gmail.com>
2024-04-28 14:12:24 +0200

    Change switch statement for if statment

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -8,15 +8,13 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
+
   playMove(move: string, amount: number) {
     let frames: number = 1
-    switch (move) {
-      case "Up":
-      case "Down":
-      case "Left":
-      case "Right":
-        frames = 18 * amount; // 18 is the amount of frames it takes to move one space in Emerald
-        break
+
+    const MULTI_MOVES: string[] = ["Up", "Down", "Left", "Right"]
+    if (MULTI_MOVES.includes(move)) {
+      frames = 18 * amount // 18 is the number of frames it takes to move one tile in Emerald
     }
 
     this.http.post(`http://localhost:5000/mgba-http/button/hold?key=${move}&duration=${frames}`, {})
