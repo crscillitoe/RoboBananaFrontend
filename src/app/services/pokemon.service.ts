197d43a1f3d54caffce1319dcdacd53c5b050a1c
ValpsZ <limelgren@gmail.com>
2024-04-28 20:29:25 +0200

    Fix hold time being to long

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -12,7 +12,7 @@ export class PokemonService {
   playMove(move: string, amount: number) {
     let frames: number = 1
     if (MULTI_MOVES.includes(move)) {
-      frames = 18 * amount // 18 is the number of frames it takes to move one tile in Emerald
+      frames = 15 * amount // 15 is the number of frames it takes to move one tile in Emerald
     }
 
     this.http.post(`http://localhost:5000/mgba-http/button/hold?key=${move}&duration=${frames}`, {})
