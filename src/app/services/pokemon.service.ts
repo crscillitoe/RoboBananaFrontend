bbfa466102f0c6f8cf56a3ee9c083cafbc1a9f4d
ValpsZ <limelgren@gmail.com>
2024-04-28 14:17:17 +0200

    Move const out of playMove

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+const MULTI_MOVES: string[] = ["Up", "Down", "Left", "Right"]
 @Injectable({
   providedIn: 'root'
 })
@@ -8,11 +9,8 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
-
   playMove(move: string, amount: number) {
     let frames: number = 1
-
-    const MULTI_MOVES: string[] = ["Up", "Down", "Left", "Right"]
     if (MULTI_MOVES.includes(move)) {
       frames = 18 * amount // 18 is the number of frames it takes to move one tile in Emerald
     }
