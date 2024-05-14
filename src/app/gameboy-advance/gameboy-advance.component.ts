c2b88dd5b4e548fa17ce4c19c1d82428c53550d7
crscillitoe <you@example.com>
2024-05-14 20:52:34 +0000

    Pokemone functional MVP

--- src/app/gameboy-advance/gameboy-advance.component.ts
+++ src/app/gameboy-advance/gameboy-advance.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import mGBA from './mgba.js';
+import mGBA, { mGBAEmulator } from './mgba.js';
+import { BotConnectorService } from '../services/bot-connector.service';
 
 @Component({
   selector: 'app-gameboy-advance',
@@ -7,7 +8,30 @@ import mGBA from './mgba.js';
   styleUrls: ['./gameboy-advance.component.scss']
 })
 export class GameboyAdvanceComponent implements OnInit {
-  ngOnInit(): void {
+  playing: boolean = false;
+
+  // Defaults to cover minimap
+  width: number = 395;
+  height: number = 261;
+  top: number = 63;
+  left: number = 294;
+
+  saveGame(module: mGBAEmulator) {
+    module.saveState(0);
+    const data = module.FS.readFile('/data/states/PokemonEmerald.ss0');
+    localStorage.setItem('saveState', "[" + data.toString() + "]");
+  }
+
+  pressKey(module: mGBAEmulator, keyName: "a" | "b" | "select" | "start" | "up" | "down" | "left" | "right" | "r" | "l") {
+    module.buttonPress(keyName);
+    setTimeout(() => {
+      module.buttonUnpress(keyName);
+    }, 500);
+  }
+
+  constructor(private botService: BotConnectorService) {}
+
+  async ngOnInit(): Promise<void> {
     let canvas_id = 'screen'
     const Module = {
       canvas: document.getElementById(canvas_id) as HTMLCanvasElement
@@ -18,36 +42,55 @@ export class GameboyAdvanceComponent implements OnInit {
     }
 
     // Define a variable of type File that references the pokemon emerald rom found in assets/PokemonEmerald.gba
-    fetch('assets/PokemonEmerald.gba').then(response => {
-      console.log(response);
-      response.blob().then(blob => {
-        blob.arrayBuffer().then(romBuffer => {
-          mGBA(Module).then(function (Module) {
-          	const mGBAVersion =
-          		Module.version.projectName +
-          		' ' +
-          		Module.version.projectVersion;
+    const pokemonROM = await fetch('assets/PokemonEmerald.gba')
+    const pokemonBLOB = await pokemonROM.blob();
+    const romBuffer = await pokemonBLOB.arrayBuffer();
+    const gbaModule = await mGBA(Module);
 
-              console.log(mGBAVersion);
+    const saveFile = await fetch('assets/PokemonEmerald.ss0');
+    const saveBLOB = await saveFile.blob();
+    const saveBuffer = await saveBLOB.arrayBuffer();
 
-          	  Module.FSInit().then(() => {
-                Module.FS.writeFile('PokemonEmerald.gba', new Uint8Array(romBuffer));
-                Module.loadGame('PokemonEmerald.gba');
+    gbaModule.FSInit().then(() => {
+       gbaModule.FS.writeFile('PokemonEmerald.gba', new Uint8Array(romBuffer));
+       if (localStorage.getItem('saveState')) {
+        gbaModule.FS.writeFile('/data/states/PokemonEmerald.ss0', new Uint8Array(JSON.parse(localStorage.getItem('saveState')!)));
+       } else {
+        gbaModule.FS.writeFile('/data/states/PokemonEmerald.ss0', new Uint8Array(saveBuffer));
+       }
 
-                for (let i = 0; i < 10; i++) {
-                  // wait 1 second
-                  setTimeout(() => {
-                    console.log('pressing start');
-                    Module.buttonPress('start');
-                    Module.buttonUnpress('start');
-                  }, 1000 * i);
-                }
-              }
-            );
-          });
-        });
-      });
-    });
+       gbaModule.loadGame('PokemonEmerald.gba');
+       gbaModule.loadState(0);
 
+       gbaModule.setVolume(0);
+
+       // No idea what mode 0 does, but 15 is clearly the number of ms between frames
+       // 1000/60 is roughly 16.666666666666668, so 15 is close enough
+       gbaModule.setMainLoopTiming(0, 15);
+
+       this.botService.getStream("streamdeck").subscribe(data => {
+        console.log(data);
+         if (data.type === 'pokemon-move' && this.playing) {
+           this.pressKey(gbaModule, data.move.toLowerCase());
+         } else if (data.type === 'pokemon-mod') {
+          if (data.value === 'save') {
+            this.saveGame(gbaModule);
+          } else if (data.value === 'off') {
+            this.saveGame(gbaModule);
+            this.playing = false;
+          } else if (data.value === 'on') {
+            this.playing = true;
+          } else if (data.value === 'load') {
+            gbaModule.loadState(0);
+          } else if (data.value === 'size') {
+            this.width = data.width;
+            this.height = data.height;
+            this.top = data.top;
+            this.left = data.left;
+          }
+         }
+       });
+     }
+    );
   }
 }
